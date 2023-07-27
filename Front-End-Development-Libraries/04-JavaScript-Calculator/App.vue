fd68213c0e46294d7ffe6a65e9b50bd519f41928
Jeffrey Cheung <139647007+CHEUNG-K-Jeffrey@users.noreply.github.com>
2023-07-27 15:47:47 -0700

    Actually add a main calculator element and style

--- Front-End-Development-Libraries/04-JavaScript-Calculator/App.vue
+++ Front-End-Development-Libraries/04-JavaScript-Calculator/App.vue
@@ -3,5 +3,16 @@
 </script>
 
 <template>
-
+    <div id="calculator">
+    </div>
 </template>
+
+<style>
+    #calculator {
+        background-color: white;
+        border: 5px solid black;
+        border-radius: 25;
+        width: 100%;
+        height: 100vh;
+    }
+</style>
