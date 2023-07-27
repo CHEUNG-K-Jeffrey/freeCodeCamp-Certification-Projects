9f0280a0f963f5f4a2d3c1c5b9bfc92e889d6810
Jeffrey Cheung <139647007+CHEUNG-K-Jeffrey@users.noreply.github.com>
2023-07-27 16:31:48 -0700

    Add calculator display

--- Front-End-Development-Libraries/04-JavaScript-Calculator/App.vue
+++ Front-End-Development-Libraries/04-JavaScript-Calculator/App.vue
@@ -1,9 +1,14 @@
 <script setup>
+import { ref } from 'vue'
+import Display from './Display.vue'
+
+const displayText = ref(0);
 
 </script>
 
 <template>
     <div id="calculator">
+    <Display>{{ displayText }}</Display>
     </div>
 </template>
 
---
+++ Front-End-Development-Libraries/04-JavaScript-Calculator/Display.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+</script>
+
+<template>
+    <div id="display"><slot></slot></div>
+</template>
+
+<style>
+    #display {
+        margin: 5vw;
+        border: 1px solid black;
+        height: 15vh;
+        text-align: right;
+        font-size: 10vh;
+    }
+</style>
