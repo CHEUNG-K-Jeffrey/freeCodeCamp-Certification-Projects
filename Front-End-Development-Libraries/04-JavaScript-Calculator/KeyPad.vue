fdec4dcba05d4bbce4eeb802ae143413b7c22966
Jeffrey Cheung <139647007+CHEUNG-K-Jeffrey@users.noreply.github.com>
2023-07-28 14:13:53 -0700

    Add all of the calculators keys listed in #10

--- Front-End-Development-Libraries/04-JavaScript-Calculator/KeyPad.vue
+++ Front-End-Development-Libraries/04-JavaScript-Calculator/KeyPad.vue
@@ -4,15 +4,32 @@ import Key from './Key.vue'
 
 <template>
     <div id="keypad">
-        <Key />
-
+        <Key id="equals">=</Key>
+        <Key id="zero">0</Key>
+        <Key id="one">1</Key>
+        <Key id="two">2</Key>
+        <Key id="three">3</Key>
+        <Key id="four">4</Key>
+        <Key id="five">5</Key>
+        <Key id="six">6</Key>
+        <Key id="seven">7</Key>
+        <Key id="eight">8</Key>
+        <Key id="nine">9</Key>
+        <Key id="add">+</Key>
+        <Key id="subtract">-</Key>
+        <Key id="multiply">*</Key>
+        <Key id="divide">/</Key>
+        <Key id="decimal">.</Key>
+        <Key id="clear">AC</Key>
     </div>
 </template>
 
 <style>
 #keypad {
+    display:grid;
     margin: 5vh;
     border: 1px solid black;
     height: 70vh;
+
 }
 </style>
