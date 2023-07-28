aa209a8bd53e36708949b64731d9b8986d24232b
Jeffrey Cheung <139647007+CHEUNG-K-Jeffrey@users.noreply.github.com>
2023-07-28 14:53:35 -0700

    Add a grid layout for KeyPad

--- Front-End-Development-Libraries/04-JavaScript-Calculator/KeyPad.vue
+++ Front-End-Development-Libraries/04-JavaScript-Calculator/KeyPad.vue
@@ -32,4 +32,87 @@ import Key from './Key.vue'
     height: 70vh;
     grid-template-columns: repeat(4, 1fr);
 }
+
+#zero {
+    grid-row: 5;
+    grid-column: 1/3
+}
+
+#decimal {
+    grid-row: 5;
+    grid-column: 3;
+}
+
+#equals {
+    grid-row: 6 / 4;
+    grid-column: 4;
+}
+
+#add {
+    grid-row: 4 / 2;
+    grid-column: 4;
+}
+
+#one {
+    grid-row: 4;
+}
+
+#two {
+    grid-row: 4;
+    grid-column: 2;
+}
+
+#three {
+    grid-row: 4;
+    grid-column: 3;
+}
+
+#four {
+    grid-row: 3;
+}
+
+#five {
+    grid-row: 3;
+    grid-column: 2;
+}
+
+#six {
+    grid-row: 3;
+    grid-column: 3;
+}
+
+#seven {
+    grid-row: 2;
+    grid-column: 1;
+}
+
+#eight {
+    grid-row: 2;
+    grid-column: 2;
+}
+
+#nine {
+    grid-row: 2;
+    grid-column: 3;
+}
+
+#clear {
+    grid-row: 1;
+    grid-column: 1
+}
+
+#divide {
+    grid-row: 1;
+    grid-column: 2;
+}
+
+#multiply {
+    grid-row: 1;
+    grid-column: 3;
+}
+
+#subtract {
+    grid-row: 1;
+    grid-column: 4;
+}
 </style>
