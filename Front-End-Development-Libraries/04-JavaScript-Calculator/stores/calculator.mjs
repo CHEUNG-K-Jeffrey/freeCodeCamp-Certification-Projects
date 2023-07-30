d8e9336ed52313ebd32184ba96f4347001bf07b7
Jeffrey Cheung <139647007+CHEUNG-K-Jeffrey@users.noreply.github.com>
2023-07-30 14:20:50 -0700

    Fix buffer error and refactor input method

--- Front-End-Development-Libraries/04-JavaScript-Calculator/stores/calculator.mjs
+++ Front-End-Development-Libraries/04-JavaScript-Calculator/stores/calculator.mjs
@@ -4,22 +4,22 @@ import { defineStore } from 'pinia'
 export const useCalculatorStore = defineStore('calculator', {
     // other options...
     state: () => {
-        return { display: "0", buffer: [] }
+        return { display: '0', buffer: '["0"]' }
     },
     getters: {
     },
     actions: {
         input(string) {
-            if (this.display === "0") {
-                this.display = string;
-            } else {
-                this.display += string;
-            }
+            let newBuffer = JSON.parse(this.buffer);
+            newBuffer.push(string)
+            this.buffer = JSON.stringify(newBuffer);
+            this.display = JSON.parse(this.buffer).join("");
         },
         calculate() {
             this.display = "CALCULATED";
         },
         reset() {
+            this.buffer = '["0"]';
             this.display = "0";
         }
     },
