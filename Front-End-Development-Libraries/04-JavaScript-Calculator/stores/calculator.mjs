4adc665e59dc1112670c2167c44a5d6b450b9d29
Jeffrey Cheung <139647007+CHEUNG-K-Jeffrey@users.noreply.github.com>
2023-07-30 17:23:18 -0700

    Refactor to use composition API and with WIP code

--- Front-End-Development-Libraries/04-JavaScript-Calculator/stores/calculator.mjs
+++ Front-End-Development-Libraries/04-JavaScript-Calculator/stores/calculator.mjs
@@ -1,27 +1,38 @@
 //stores/calculator.js
+import { computed, ref } from 'vue'
 import { defineStore } from 'pinia'
 
-export const useCalculatorStore = defineStore('calculator', {
-    // other options...
-    state: () => {
-        return { display: '0', buffer: '["0"]' }
-    },
-    getters: {
-    },
-    actions: {
-        input(string) {
-            // Only numbers, decimals and operators are expected.
-            let newBuffer = JSON.parse(this.buffer);
-            newBuffer.push(string)
-            this.buffer = JSON.stringify(newBuffer);
-            this.display = JSON.parse(this.buffer).join("");
-        },
-        calculate() {
-            this.display = "CALCULATED";
-        },
-        reset() {
-            this.buffer = '["0"]';
-            this.display = "0";
+export const useCalculatorStore = defineStore('calculator', () => {
+    const display = computed(() => buffer.value.join(""))
+    const buffer = ref(["0"]);
+
+    function input(string) {
+        let lastOffset = buffer.value.length - 1;
+        //Process number
+        if (/[0-9\.]/.test(string)) {
+            //Check for leading zero
+            if (buffer.value[lastOffset] === "0") {
+                buffer.value[lastOffset] = string;
+                return;
+            }
+            //Check for decimal
+            if (string === ".") {
+                if (/\./.test(buffer.value[lastOffset]) === false) {
+                    buffer.value[lastOffset] = buffer.value[lastOffset].concat(string);
+                }
+                return;
+            }
+            buffer.value[lastOffset] = buffer.value[lastOffset].concat(string);
         }
-    },
+
+    }
+    function calculate() {
+        //Check for number only string and convert to number and compute
+        buffer.value = ["CALCULATED"];
+    }
+    function reset() {
+        buffer.value = ["0"];
+    }
+
+    return { display, buffer, input, calculate, reset }
 })
