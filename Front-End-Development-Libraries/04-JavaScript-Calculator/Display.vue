03a9eabe205c89aeea4f4c8a6f5e8bc8d71cdb42
Jeffrey Cheung <139647007+CHEUNG-K-Jeffrey@users.noreply.github.com>
2023-07-31 16:56:52 -0700

    Change font size to use 2em

--- Front-End-Development-Libraries/04-JavaScript-Calculator/Display.vue
+++ Front-End-Development-Libraries/04-JavaScript-Calculator/Display.vue
@@ -25,6 +25,6 @@ watch(store, (store, prevStore) => {
         border: 1px solid black;
         height: 15vh;
         text-align: right;
-        font-size: 10vh;
+        font-size: 2em;
     }
 </style>
