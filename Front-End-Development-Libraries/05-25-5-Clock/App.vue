f4b0a3faef5eeeb5e59fd23e452d51f7a23e6f38
Jeffrey Cheung <139647007+CHEUNG-K-Jeffrey@users.noreply.github.com>
2023-08-02 15:42:12 -0700

    Basic UI WIP

--- Front-End-Development-Libraries/05-25-5-Clock/App.vue
+++ Front-End-Development-Libraries/05-25-5-Clock/App.vue
@@ -9,6 +9,10 @@ store.startTimer();
 </script>
 
 <template>
+    <div>25 + 5 Clock</div>
+    <div>Session Length</div>
+    <div>Break Length</div>
+    <div>Session {{ store.count }}</div>
 
 </template>
 
