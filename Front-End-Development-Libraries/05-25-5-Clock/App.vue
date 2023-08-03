8cb5b8961f969fe1fab1ecc2f71a2e40bf570844
Jeffrey Cheung <139647007+CHEUNG-K-Jeffrey@users.noreply.github.com>
2023-08-03 10:05:14 -0700

    Handle click event for up and down arrows

--- Front-End-Development-Libraries/05-25-5-Clock/App.vue
+++ Front-End-Development-Libraries/05-25-5-Clock/App.vue
@@ -10,15 +10,15 @@ const store = useTimerStore();
     <h1>25 + 5 Clock</h1>
     <div>
         <h2 id="session-label">Session Length</h2>
-        <i class="fa-solid fa-chevron-up"></i>
+        <i @click="store.incrementSession" class="fa-solid fa-chevron-up"></i>
         <div id="session-length">{{ store.sessionTime }}</div>
-        <i class="fa-solid fa-chevron-down"></i>
+        <i @click="store.decrementSession" class="fa-solid fa-chevron-down"></i>
     </div>
     <div>
         <h2 id="break-label">Break Length</h2>
-        <i class="fa-solid fa-chevron-up"></i>
+        <i @click="store.incrementBreak" class="fa-solid fa-chevron-up"></i>
         <div id="break-length">{{ store.breakTime }}</div>
-        <i class="fa-solid fa-chevron-down"></i>    </div>
+        <i @click="store.decrementBreak" class="fa-solid fa-chevron-down"></i>    </div>
     <div>
         <h2 id="timer-label">Current
             <span v-show="store.sessionType === 'session'">Session</span>
